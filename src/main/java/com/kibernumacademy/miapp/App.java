859efb15ac68a1d4d6ce569d904c0ceb2bfa193d
ToboCodes <ccontreras.cumin@gmail.com>
2023-05-18 19:42:25 -0400

    test: Added basic tests

--- src/main/java/com/kibernumacademy/miapp/App.java
+++ src/main/java/com/kibernumacademy/miapp/App.java
@@ -1,9 +1,30 @@
 package com.kibernumacademy.miapp;
 
-public class App {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public static void main(String[] args) {
-	System.out.println("Hola a todos los del bootcamp DevOps 082");
-}
+public class App {
+	
+	public static int calculateDoubleEvens(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(n -> n * 2)
+                .sum();
+    }
+
+    public static List<String> convertToUpperCase(List<String> strings) {
+        return strings.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+    }
+
+	public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("La suma es: " + calculateDoubleEvens(numbers));
+
+        List<String> strings = Arrays.asList("hola", "mundo", "java", "8");
+        System.out.println("Las palabras en mayúsculas son: " + convertToUpperCase(strings));
+    }
 
 }
